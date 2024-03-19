0df4d8d8a2b9919cd4bee6a7027434cdf4d89313
tate_Code <ashwintate111@gmail.com>
2024-03-19 11:06:22 +0530

    Challage one to four added on 19-03-24

---
+++ demo/src/main/java/com/example/demo/ChFour.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+//Show the patterns using single print statement
+public class ChFour {
+
+	public static void main(String[] args) {
+		System.out.println("Left Hand star pattern\n");
+		System.out.println("*\n**\n***\n****\n*****" );
+		System.out.println("Right Hand star pattern\n");
+		System.out.println("    *\n   **\n  ***\n ****\n*****" );
+		System.out.println("Dwon Hand star pattern\n");
+		System.out.println("*****\n**** \n***  \n**   \n*    " );
+
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/Ticket.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Ticket {
+
+	int id;
+	String name;
+	float price; 
+	
+	Ticket(int id)
+	{
+		this.id=id; 
+	}
+	public static void main(String[] args) {
+		
+	//	Ticket t1 = new Ticket(10);
+	}
+	
+}
